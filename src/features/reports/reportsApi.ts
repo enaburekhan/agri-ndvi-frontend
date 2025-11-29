1c29b526e2c58555cf49628b42fe0dbd84a047d0
enaburekhan <ericenaburekhan2018@gmail.com>
2025-11-29 21:54:05 +0000

    Update reportsApi endpoints

--- src/features/reports/reportsApi.ts
+++ src/features/reports/reportsApi.ts
@@ -1,33 +1,29 @@
 import { api } from "../../app/api";
-import type { CreateReportDto, Report } from "../../types/report";
+import type { Report } from "../../types/report";
 
 export const reportsApi = api.injectEndpoints({
   endpoints: (builder) => ({
     // Get all reports
-    getReports: builder.query<Report[], void>({
-      query: () => "/reports",
-      providesTags: ["Reports"],
+    getProjectReports: builder.query<Report[], number>({
+      query: (projectId) => `/projects/${projectId}/reports`,
+      providesTags: (result, _error, projectId) => 
+        result 
+          ? [
+              ...result.map((r) => ({ type: "Reports" as const, id: r.id })),
+              { type: "Reports", id: `PROJECT_${projectId}`}
+            ] 
+          : [{ type: "Reports", id: `PROJECT_${projectId}`}]
     }),
 
     // Get single report by ID
     getReportById: builder.query<Report, number>({
       query: (id) => `/reports/${id}`,
-      providesTags: (_result, _error, id) => [{ type: "Reports", id }],
-    }),
-    // Create a new report
-    createReport: builder.mutation<Report, CreateReportDto>({
-      query: (body) => ({
-        url: "/reports",
-        method: "POST",
-        body,
-      }),
-      invalidatesTags: ["Reports"],
+      providesTags: (_res, _err, id) => [{ type: "Reports", id }],
     }),
   }),
 });
 
 export const {
-  useGetReportsQuery,
+  useGetProjectReportsQuery,
   useGetReportByIdQuery,
-  useCreateReportMutation,
 } = reportsApi;
